e1374ea33352a962214a281267cf5c8f006448bf
David Vega Regollo <davidve@gmail.com>
2021-07-08 02:22:50 -0400

    Fix validación string

--- src/Rut.php
+++ src/Rut.php
@@ -194,7 +194,7 @@ class Rut
         if ($dv===null) {
             list($rut, $dv) = self::separar($rut);
         }
-        if (!is_numeric($rut) {
+        if (!is_numeric($rut)) {
             return false;
         }
         return ($dv == self::calcularVerificador($rut));
